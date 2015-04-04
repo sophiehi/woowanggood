68c6487bdf73bae8c673f7874f9ea864895e7b1b
Sukwon Choi <swchoi06@wafflestudio.com>
2015-04-04 17:27:42 +0900

    Applied Intellij

---
+++ src/com/woowanggood/Server.java
@@ -0,0 +1,44 @@
+package com.woowanggood;
+
+/**
+ * Created by swchoi06 on 4/4/15.
+ */
+/* Server
+usag: java Server [RTSP listening port]
+---------------------- */
+
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Server{
+    //------------------------------------
+    //main
+    //------------------------------------
+    public static void main(String argv[]) throws Exception
+    {
+        System.out.println("Server started");
+
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+
+        //get RTSP socket port from the command line
+        int RTSPport = 3000;
+
+        try {
+            serverSocket = new ServerSocket(RTSPport);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (true) {
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                System.out.println("I/O error: " + e);
+            }
+            // new threa for a client
+            new RTSPThread(socket).start();
+        }
+    }
+}
