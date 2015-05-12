2fcfb795cd163e62bb2b2345ab04d257e4d7f4af
sophiechoi <woojungc@gmail.com>
2015-05-12 13:03:52 +0900

    Implement resource monitor roughly

--- src/com/woowanggood/ResourceMonitor.java
+++ src/com/woowanggood/ResourceMonitor.java
@@ -1,44 +1,98 @@
 package com.woowanggood;
 
+import com.sun.management.OperatingSystemMXBean;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.net.Socket;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import static java.util.concurrent.TimeUnit.*;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by SophiesMac on 15. 5. 10..
  */
+//todo ThreadHandler에서 ResourceMonitor를 호출.
 public class ResourceMonitor {
-    private double CPUUsage;
-    private double memoryUsage;
+    private final String ADDRESS_LOADBALANCER = "localhost";
+    private final int PORT_LOADBALANCER = 2000;//todo 같은 서버소켓으로 보내도 되나?
 
-    private final String ADDRESS_LOADBALANCER = "0.0.0.0";
-    private final String PORT_LOADBALANCER = "0000";
+    private OperatingSystemMXBean osBean =
+            (com.sun.management.OperatingSystemMXBean)
+                    ManagementFactory.getOperatingSystemMXBean();
 
-    public ResourceMonitor(){
-        this(0.0, 0.0);
+    /** process */
+    private double processCPUpercent ;
+    private long availableVMsize ;
+
+    /** system */
+    private double systemCPUpercent ;
+    private double systemPMpercent ;
+
+    /** num of clients or sessions*/
+    private int numOfThreads;
+    private int numOfSessions;
+
+    /** socket & outputStream */
+    private Socket socket;
+    private DataOutputStream dosWithServer;
+
+    /** scheduler */
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    public ResourceMonitor() throws IOException {
+        this.socket = new Socket(ADDRESS_LOADBALANCER, PORT_LOADBALANCER);
+        this.dosWithServer = new DataOutputStream(socket.getOutputStream());
+
+        this.processCPUpercent = osBean.getProcessCpuLoad();
+        this.availableVMsize  = osBean.getCommittedVirtualMemorySize();
+        this.systemCPUpercent = osBean.getSystemCpuLoad();
+        this.systemPMpercent  = osBean.getFreePhysicalMemorySize() / osBean.getTotalPhysicalMemorySize();
     }
 
-    public ResourceMonitor(double CPUUsage, double memoryUsage){
-        this.CPUUsage = CPUUsage;
-        this.memoryUsage = memoryUsage;
+    public void updateAndReport() {
+        final Runnable beeper = new Runnable() {
+            public void run() {
+                updateInfo();
+                try {
+                    reportToLoadbalancerPeriodically();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-        //todo Q. JVM ? or OS ? >> I think OS, not JVM.
-        //https://support.hyperic.com/display/SIGAR/Home //시스템 정보 .
-        //해당 프로세스? oo 스레드? xx
-        //http://stackoverflow.com/questions/74674/how-to-do-i-check-cpu-and-memory-usage-in-java
-        //http://helloworld.naver.com/helloworld/textyle/184615
+                System.out.println("beep");
+            }
+        };
+
+        final ScheduledFuture<?> beeperHandle =
+                scheduler.scheduleAtFixedRate(beeper, 10, 1, SECONDS);
+
+        //todo 1초가 아니라 1회로 하려면?
+        scheduler.schedule(new Runnable() {
+            public void run() { beeperHandle.cancel(true); }
+        }, 1, SECONDS);
     }
 
+    public void run(){
+        updateAndReport();
+    }
 
     //@Scheduled
     public void updateInfo(){
-        //update this.CPUUsage, this.memoryUsage
+        System.out.println("update this.CPUUsage, this.memoryUsage");
+        this.processCPUpercent = osBean.getProcessCpuLoad();
+        this.availableVMsize = osBean.getCommittedVirtualMemorySize();
+        this.systemCPUpercent = osBean.getSystemCpuLoad();
+        this.systemPMpercent  = osBean.getFreePhysicalMemorySize() / osBean.getTotalPhysicalMemorySize();
     }
 
     //@Scheduled
-    public void report(){
-        //send this.CPUUsage, this.memoryUsage to LoadBalancer C
+    public void reportToLoadbalancerPeriodically() throws IOException {
+        dosWithServer.writeUTF("hello from Resourse Monitor");//outputstream타고 socket타고 나감.
     }
-
 }
 
-
-
 //not yet: Computer A의 전원 꺼졌을때, B에서 재개하기 (latency 2초 목표?)
-
